f12d8fe96d8009699a16cbf9cc66e4c18e6e030b
MarcoBasileDev <marco.basile1996@gmail.com>
2024-04-07 12:25:10 +0200

    add show counter logic

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,13 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
-    return { counter: state.counter + 1 };
+    return {
+      counter: state.counter + 1,
+      showCounter: state.showCounter,
+    };
   }
 
   if (action.type === "increase") {
@@ -12,7 +17,17 @@ const counterReducer = (state = { counter: 0 }, action) => {
   }
 
   if (action.type === "decrement") {
-    return { counter: state.counter - 1 };
+    return {
+      counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
+    };
   }
 
   return state;
