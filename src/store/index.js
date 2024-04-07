a5a23807f5b6e84bc5168adf7fe6a33f480fbd8d
MarcoBasileDev <marco.basile1996@gmail.com>
2024-04-07 11:52:32 +0200

    fix counterReducer function

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import { createStore } from "redux";
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  if (action === "increment") {
+  if (action.type === "increment") {
     return { counter: state.counter + 1 };
   }
 
-  if (action === "decrement") {
+  if (action.type === "decrement") {
     return { counter: state.counter - 1 };
   }
 
